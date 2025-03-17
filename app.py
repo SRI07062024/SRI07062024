d523c7227dbb8b0194a26612073aa3bd3f20d6f6
SRI07062024 <sri.konduru13@gmail.com>
2025-03-17 18:14:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,27 +52,7 @@ if default_module:
     st.text_input("Module", default_module, disabled=True)
     selected_module = default_module
 else:
-    # selected_module = st.selectbox("Select Module", available_modules)
-
-    col1, col2 = st.columns([1, 1])  # Two equal-width columns (50% each)
-
-    with col1:
-        selected_module = st.selectbox("Select Module", available_modules, key="module_select")
-    # ✅ Fetch Portfolio Name matching selected Module
-    def fetch_portfolio_name(selected_module):
-        df = session.table("Override_Ref").to_pandas()
-        df.columns = [col.upper() for col in df.columns]
-        module_num = int(selected_module.split('-')[1])
-        portfolio_name = df[df['MODULE'] == module_num]['PORTFOLIO_NAME'].unique()
-        return portfolio_name[0] if len(portfolio_name) > 0 else "N/A"
-
-    portfolio_name = fetch_portfolio_name(selected_module)
-
-    with col2:
-        st.text_input("Portfolio Name", portfolio_name, disabled=True)  # Show matched Portfolio Name
-
-    # with col2:
-    #     st.write("")  # Keeps second column empty for spacing
+    selected_module = st.selectbox("Select Module", available_modules)
 
 # ✅ Fetch override ref data for the selected module
 def fetch_override_ref_data(selected_module):
